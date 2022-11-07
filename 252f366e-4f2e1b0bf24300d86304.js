8366ef7356e065fb0a7a502cc58117f2ecead8fc
shj <josh2kv@gmail.com>
2022-11-07 22:28:24 +0900

    Updates

---
+++ 252f366e-4f2e1b0bf24300d86304.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[228],{5827:function(t,a,e){e.d(a,{SEL:function(){return h}});var r=e(5566);function h(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M6 17h3l2-4V7H5v6h3zm8 0h3l2-4V7h-6v6h3z"}}]})(t)}}}]);
+//# sourceMappingURL=252f366e-4f2e1b0bf24300d86304.js.map
